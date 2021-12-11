11b16848e56918b9ae5c1d07a15088cae8721fe0
David Lizon <draggero@gmail.com>
2021-12-10 17:03:40 -0700

    Create SpringBoot/MVC project

---
+++ EternityGameHub/src/main/java/com/skilldistillery/eternitygamehub/EternityGameHubApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.eternitygamehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternityGameHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EternityGameHubApplication.class, args);
+	}
+
+}
