899abdb5efb3ba23a90f2e374f375baa4cebdeec
CJ Harris <Carroll.Harris2@gmail.com>
2021-12-12 11:36:10 -0500

    created Game entity

--- JPAGameHub/src/main/java/com/skilldistillery/eternitygamehub/entities/Game.java
+++ JPAGameHub/src/main/java/com/skilldistillery/eternitygamehub/entities/Game.java
@@ -1,5 +1,8 @@
 package com.skilldistillery.eternitygamehub.entities;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,4 +14,81 @@ public class Game {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	private String title;
+	
+	private String description;
+	
+	@Column(name = "max_players")
+	private int maxPlayers;
+	
+	@Column(name = "image_url")
+	private String gameImageUrl;
+	
+	public Game() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getMaxPlayers() {
+		return maxPlayers;
+	}
+
+	public void setMaxPlayers(int maxPlayers) {
+		this.maxPlayers = maxPlayers;
+	}
+
+	public String getGameImageUrl() {
+		return gameImageUrl;
+	}
+
+	public void setGameImageUrl(String gameImageUrl) {
+		this.gameImageUrl = gameImageUrl;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", title=" + title + ", description=" + description + ", maxPlayers=" + maxPlayers
+				+ ", gameImageUrl=" + gameImageUrl + "]";
+	}
+	
+	
 }
