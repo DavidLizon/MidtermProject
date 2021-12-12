c98c98d47ddb79bb1d5c47fa8f03f3585577fc17
CJ Harris <Carroll.Harris2@gmail.com>
2021-12-12 12:08:54 -0500

    created Genre entity

--- JPAGameHub/src/main/java/com/skilldistillery/eternitygamehub/entities/GameInventory.java
+++ JPAGameHub/src/main/java/com/skilldistillery/eternitygamehub/entities/GameInventory.java
@@ -7,8 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "game_inventory")
 public class GameInventory {
 
 	@Id
