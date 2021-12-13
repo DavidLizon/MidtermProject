dc01c2a5a519d18036799119c4cb7e32e8b7b6d1
Ryan Majors <growbox.ryan@yahoo.com>
2021-12-13 14:02:42 -0700

    Added findByKeyword.do RequestMapping method

--- EternityGameHub/src/main/java/com/skilldistillery/eternitygamehub/controllers/HomeController.java
+++ EternityGameHub/src/main/java/com/skilldistillery/eternitygamehub/controllers/HomeController.java
@@ -1,11 +1,16 @@
 package com.skilldistillery.eternitygamehub.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.skilldistillery.eternitygamehub.data.UserDAO;
+import com.skilldistillery.eternitygamehub.entities.GameInventory;
 
 @Controller
 public class HomeController {
@@ -14,8 +19,18 @@ public class HomeController {
 	private UserDAO userDao;
 	
 	@RequestMapping(path= {"/", "home.do"})
-	public String home(Model model) {
-		model.addAttribute("DEBUG", userDao.findByUsername("admin"));
+	public String home() {
 		return "home";
 	}
+	
+	@RequestMapping(path= "findByKeyword.do", method = RequestMethod.GET)
+	public String findByKeyword(String keyword, Model model) {
+		List<GameInventory> games = new ArrayList<>();
+		games = userDao.findGameByKeywordSearch(keyword);
+		model.addAttribute("result", games);
+		return "search";
+		
+	}
+	
+	
 }
