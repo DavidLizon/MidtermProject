feeb96b48c2815aab7f285204720a9ce8a631e54
Ryan Majors <growbox.ryan@yahoo.com>
2021-12-14 10:28:59 -0700

    Added @Controller annotation to gamecontroler

--- EternityGameHub/src/main/java/com/skilldistillery/eternitygamehub/controllers/GameController.java
+++ EternityGameHub/src/main/java/com/skilldistillery/eternitygamehub/controllers/GameController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import com.skilldistillery.eternitygamehub.data.GameDAO;
 import com.skilldistillery.eternitygamehub.entities.Game;
 import com.skilldistillery.eternitygamehub.entities.GameInventory;
 
+@Controller
 public class GameController {
 	
 	@Autowired
